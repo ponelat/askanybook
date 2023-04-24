3b08806bb1f75579a7546e814f8c14472a7428c3
Josh Ponelat <jponelat@gmail.com>
2023-04-24 18:16:04 +0200

    infra: add embeddings.to_csv_s and from_csv_s

--- lib/shared/embeddings.rb
+++ lib/shared/embeddings.rb
@@ -1,6 +1,35 @@
 class Embeddings
+  def self.from_csv_s(csv_s)
+    rows = CSV.parse(csv_s, headers: true)
+    embeddings = rows.map do |row|
+      id = row['id']
+      content = row['content']
+      embedding = row['embedding']
+      [id, content, embedding.split(';').map {|n| Float(n)}]
+    end
+    Embeddings.new(embeddings)
+  end
+
   def self.similarity(a,b)
     self.cosine_similarity(a,b)
+  end
+
+  def initialize(table)
+    @table = table
+  end
+
+  def length()
+    @table.length
+  end
+
+  def to_csv_s()
+    CSV.generate do |csv|
+      csv << ['id', 'content', 'embedding']
+      @table.map do |row|
+        id, content, embedding = row.values_at(:id, :content, :embedding)
+        csv << [id, content, embedding.join(';')]
+      end
+    end
   end
 
   # Credit: ChatGPT, modified
--- test/lib/embeddings_test.rb
+++ test/lib/embeddings_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class EmbeddingsTest <  ActiveSupport::TestCase
   require 'shared/embeddings'
@@ -10,16 +10,40 @@ class EmbeddingsTest <  ActiveSupport::TestCase
   far_a = Embeddings.normalize_vector([1,999])
   far_b = Embeddings.normalize_vector([1,900])
 
-  test ".similarity near_a -> near_b greater than near_a -> far_a" do
+  # Contains IDs: apple, banana, ostrich, giraffe, newspaper, fruit, animal, yellow.
+  # The last three (fruit, animal, yewllo) are treated like questions.
+  # basic_fixture = CSV.read('../fixtures/basic.embeddings.csv', headers: true)
+
+
+  test '.similarity near_a -> near_b greater than near_a -> far_a' do
     assert Embeddings.similarity(near_a, near_b) > Embeddings.similarity(near_a, far_a)
   end
 
-  test ".similarity near_b -> near_a greater than near_b -> far_a" do
+  test '.similarity near_b -> near_a greater than near_b -> far_a' do
     assert Embeddings.similarity(near_b, near_a) > Embeddings.similarity(near_b, far_a)
   end
 
-  test ".similarity far_a -> far_b greater than far_a -> near_a" do
+  test '.similarity far_a -> far_b greater than far_a -> near_a' do
     assert Embeddings.similarity(far_a, far_b) > Embeddings.similarity(far_a, near_a)
   end
+
+  test '.new should accept a list of maps' do
+    one = {id: '1', content: 'one', embedding: [0.1, -0.9]}
+    two = {id: '2', content: 'two', embedding: [0.1, -0.9]}
+    embeds = Embeddings.new([one, two])
+    assert_equal(embeds.length, 2)
+  end
+
+  test '#to_csv_s should produce string CSV' do
+    apple = {id: 'a', content: 'apple', embedding: [0.1, -0.900000000000003]}
+    embeds = Embeddings.new([apple])
+    assert_equal("id,content,embedding\na,apple,0.1;-0.900000000000003\n", embeds.to_csv_s) # Not sure about the extra newline, but CSV.generate adds it so...
+  end
+
+  test '.from_csv_s should hydrate a new Embeddings from string CSV' do
+    embeds = Embeddings.from_csv_s( "id,content,embedding\na,apple,0.1;-0.9\n")
+    assert_equal(embeds.length, 1)
+  end
+
 
 end
