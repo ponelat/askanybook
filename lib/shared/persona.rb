70c74ac270cc8e7f25c8aa1e94ce9cf1f42ac48c
Josh Ponelat <jponelat@gmail.com>
2023-04-27 18:47:43 +0200

    infra: add Persona.build_prompt

--- lib/shared/persona.rb
+++ lib/shared/persona.rb
@@ -5,16 +5,29 @@ require 'active_model'
 class Persona
   include ActiveModel::Model
   include ActiveModel::Attributes
-  # include ActiveModel::Validations
 
   attribute :template, :string, default: '$$context $$question'
+  attribute :template_tokens, :string, default: '$$context $$question'
+  # TODO: Use proper class types
+  attribute :embeds
 
-  # # TODO: Add Embeddings type
-  # attribute :embeddings
-  # validates :embeddings, presence: true
+  def initialize(embeds:, template:)
+    @embeds = embeds
+    @template = template
+    template_without_placeholders = Persona.fill_template(template, '', '')
+    @template_tokens = Tokenizer.count_tokens(template_without_placeholders)
+  end
 
   # TODO: Handle double-replacing (in the event that "context" includes the token "$$question")
   def self.fill_template(str, context, question)
     str.gsub('$$question', question).gsub('$$context', context)
   end
+
+  def build_prompt(question_embed, max_tokens)
+    tokens_left = max_tokens - question_embed.tokens - @template_tokens
+    context = @embeds.get_best_context_for(question_embed.embedding, tokens_left)
+    question = question_embed.content
+    Persona.fill_template @template, context, question
+  end
+
 end
--- test/lib/persona_test.rb
+++ test/lib/persona_test.rb
@@ -5,8 +5,37 @@ require 'test_helper'
 class PersonaTest < ActiveSupport::TestCase
   require 'shared/persona'
 
+  one = Embeds.normalize_vector([1,1])
+  two = Embeds.normalize_vector([1,2])
+  three = Embeds.normalize_vector([1,3])
+  four = Embeds.normalize_vector([1,4])
+
+  # content = Embeds.from_csv_s(File.read('test/fixtures/files/basic.content.embeds.csv'))
+  # subjects = Embeds.from_csv_s(File.read('test/fixtures/files/basic.subjects.embeds.csv'))
+  embeds = Embeds.new(
+    [{ id: 'one   ', tokens: 1, content: 'one', embedding: one },
+     { id: 'two   ', tokens: 1, content: 'two', embedding: two },
+     { id: 'three ', tokens: 1, content: 'three', embedding: three },
+     { id: 'four  ', tokens: 1, content: 'four', embedding: four }]
+  )
+
   test '.fill_template should replace $$content and $$question' do
     str = Persona.fill_template('hello $$context and $$question', 'foo', 'bar')
     assert_equal 'hello foo and bar', str
   end
+
+  test 'build_prompt should take an embedding and return prompt' do
+    persona = Persona.new(embeds: embeds, template: 'Intro. $$context. $$question')
+
+    question_embed = Embed.new(
+      id: 'five',
+      content: 'five__',
+      tokens: 1,
+      embedding: Embeds.normalize_vector([1, 5])
+    )
+
+    prompt = persona.build_prompt(question_embed, 6)
+    assert_equal 'Intro. four three two . five__', prompt
+  end
+
 end
