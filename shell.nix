74a1356fe26cab68e28187945c2f0cf4d986f79c
Josh Ponelat <jponelat@gmail.com>
2023-04-27 10:43:20 +0200

    infra: failed to hack tiktoken_ruby with nix. Moving on

--- shell.nix
+++ shell.nix
@@ -7,6 +7,7 @@ let
     gemset = ./nix/gemset.nix;
 
     gemConfig = pkgs.defaultGemConfig // {
+
       sqlite3 = attrs: {
           nativeBuildInputs = [ pkg-config ];
           buildInputs = [ sqlite ];
@@ -14,8 +15,29 @@ let
             "--enable-system-libraries"
           ];
         };
-    };
 
+    #   tiktoken_ruby = attrs: {
+    #     # preBuild = ''
+    #     preInstall = ''
+    #         echo Howdy
+    #         # mkdir home
+    #         # export RUSTUP_HOME=$PWD/home
+    #         # rustup override set 1.50.0
+    #         # rustup default stable
+    #         # rustup install stable
+    #         # rustup default stable
+    #     '';
+    #     nativeBuildInputs = with pkgs; [
+    #       rustc
+    #       cargo
+    #       # rustup
+    #       # clang
+    #       # llvmPackages.bintools
+    #     ];
+    #     dependencies = [ "rb_sys" ];
+    #   };
+
+    };
   };
 
 in
@@ -26,8 +48,6 @@ pkgs.mkShell {
     rubyEnv
     rubyEnv.wrappedRuby
     pkgs.entr
-    pkgs.nodejs
-    pkgs.yarn
   ];
 
 }
