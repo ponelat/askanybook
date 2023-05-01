8b108a8752a3343079c427fd02871f8b59e1501b
Josh Ponelat <jponelat@gmail.com>
2023-05-01 16:40:10 +0200

    feat: add question textarea and answer it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,61 @@
-import {useEffect} from 'react'
+import {useCallback, useEffect, useRef} from 'react'
 import ErrorPage from './ErrorPage'
 import useHttp from './useHttp'
+import QuestionInput from './QuestionInput'
 
 function App() {
+  const inputRef = useRef(null)
+
   const [healthState, checkHealth] = useHttp('/health')
-  const isUnhealthy = healthState.matches('error')
+
+  const [questionState, fetchAnswer] = useHttp(question => ({
+    url: '/ask',
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ question })
+  }))
+
+  // Prefer this function, over directly passing fetchAnswer to a component. So we know what props we're dealing with.
+  const askQuestion = useCallback((question) => fetchAnswer(question), [fetchAnswer])
 
   useEffect(() => {
     checkHealth()
   }, [checkHealth])
 
-  if(isUnhealthy) {
+  // Focus cursor at the _end_ of the question input
+  useEffect(() => {
+    if(inputRef.current) {
+      inputRef.current.focus()
+      const inputLength = inputRef.current.value.length
+      inputRef.current.setSelectionRange(inputLength, inputLength);
+    }
+  }, [inputRef])
+
+
+  // TODO: Move the health check into a router
+  if(healthState.matches('error')) {
     return <ErrorPage {...healthState.context} unexpected="GET /health came back negative" />
   }
 
   return (
-    <div className="p-4 text-gray-800" >
-      Lets go
+    <div className="p-4 text-gray-800 bg-gray-50 dark:bg-gray-700" >
+      <QuestionInput ref={inputRef} onAsk={askQuestion}/>
+      <div className="py-3 px-4" >
+
+	{questionState.matches('loading') ? (
+          <span>Loading...</span>
+	) : null}
+
+	{questionState.matches('error') ? (
+          <ErrorPage {...questionState.context} />
+	) : null}
+
+	{questionState.matches('success') ? (
+          <div>
+            <b>Answer:</b> {questionState.context.body.answer}
+          </div>
+	) : null}
+      </div>
     </div>
   );
 }
---
+++ frontend/src/QuestionInput.jsx
@@ -0,0 +1,59 @@
+import {useState, useCallback, forwardRef } from 'react'
+import TextareaAutosize from 'react-textarea-autosize'
+
+export default forwardRef(({ onAsk, isLoading=false }, ref) => {
+
+  const [input, setInput] = useState('What is this book about? ')
+  const onSubmit = useCallback(() => onAsk(input), [input, onAsk])
+
+  // Shift enter to submit
+  const handleKeyPress = useCallback((event) => {
+    if(!input)
+      return
+    if (event.key === 'Enter' && (event.shiftKey || event.ctrlKey)) {
+      event.preventDefault();
+      onSubmit(input)
+    }
+  }, [input, onSubmit])
+
+  return (
+    
+    <form onSubmit={e => { e.preventDefault(); console.log('hi'); onSubmit(input) }}>
+      <label htmlFor="chat" className="sr-only">
+	A question
+      </label>
+      <div className="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 focus:ring-0 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
+	<TextareaAutosize
+          ref={ref}
+	  id="chat"
+          minRows={1}
+          onKeyPress={handleKeyPress}
+	  className="m-0 w-full resize-none border-0 bg-transparent p-0 pr-11 focus:ring-0 focus-visible:ring-0 outline-none dark:bg-transparent pl-2 md:pl-0"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+	  placeholder="Ask a question..."
+	/>
+	<button
+	  type="submit"
+          disabled={!input.length}
+	  className="absolute p-1 rounded-md text-gray-500 bottom-1.5 md:bottom-2.5 hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-1 md:right-2 disabled:opacity-40"
+	>
+	  <svg
+	    aria-hidden="true"
+	    className="w-6 h-6 rotate-45"
+	    fill="currentColor"
+	    viewBox="0 0 20 20"
+	    xmlns="http://www.w3.org/2000/svg"
+	  >
+	    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+	  </svg>
+	  <span className="sr-only">Send message</span>
+	</button>
+
+      </div>
+      <div className="text-gray-400 text-sm pt-0.5 text-center">Shift- or Ctrl-Enter to ask</div>
+    </form>
+
+  )
+})
+
